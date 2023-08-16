f531cbd6a5d7057d79b413248be41a9f700f0c00
marlon <marlonsuyuc@gmail.com>
2023-08-15 19:23:37 -0600

    add colos background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        modeDarkTitle: '#E2E8F0',
-        modeDarkParrafo: '#94A3B8',
-        modeDarkBg: '#0F172A'
+        modeDarkTitle: "#E2E8F0",
+        modeDarkParrafo: "#94A3B8",
+        modeDarkBg: "#0F172A",
       },
       backgroundImage: {
-        'blue-gradient-radial': 'radial-gradient(circle,#0141ff 0,rgba(161,252,70,0) 71%)'
-      }
-    }
+        "green-gradient-radial":
+          "radial-gradient(circle, #ccc 0,rgba(161,252,70,0) 71%)",
+        "blue-gradient-radial":
+          "radial-gradient(circle,#0141ff 0,rgba(161,252,70,0) 71%)",
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
+// #DB2777
